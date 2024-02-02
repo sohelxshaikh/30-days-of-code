2bb587237e9745885a9ef97446491e09739cda7c
sohelxshaikh <sohailhacker7786@gmail.com>
2024-02-02 22:26:12 +0530

    new file:   Day2.cpp 	new file:   Day2.exe

---
+++ Day2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name ;
+    int age ;
+    cout<<"Enter name:";
+    cin>>name;
+    cout<<"Enter age:";
+    cin>>age;
+    cout<<"Hello my name is "<<name<<" and Age is "<<age;
+
+    return 0;
+}
